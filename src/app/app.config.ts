4d63375ed5170042ad9e852e9f8aa128a2fd1ea5
Runar Nazmutdinov <runarnazmutdinov@gmail.com>
2025-07-27 20:14:01 +0300

    fix: delete store module

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,11 @@
-import { ApplicationConfig, importProvidersFrom, ENVIRONMENT_INITIALIZER, Provider } from '@angular/core';
-import { StoreModule } from '@ngrx/store';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { userReducer } from './store/users/user.reducer';
+
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    {
-      provide: ENVIRONMENT_INITIALIZER,
-      multi: true,
-      useValue: () => {
-        // Любая твоя инициализация
-
-        console.log('Приложение инициализируется🚀');
-      }
-    },
     provideRouter(routes)
     , provideHttpClient()
   ]
